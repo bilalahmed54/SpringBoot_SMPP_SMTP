685c17ac364b610d2c04e1f1cc68806e3bb4b5db
admin <admin>
2018-05-12 14:26:48 +0500

    Base Project Setup Created.

---
+++ src/main/java/com/bay/springbootsmtpsmpp/SpringbootsmtpsmppApplication.java
@@ -0,0 +1,12 @@
+package com.bay.springbootsmtpsmpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsmtpsmppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsmtpsmppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bay/springbootsmtpsmpp/SpringbootsmtpsmppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bay.springbootsmtpsmpp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootsmtpsmppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
